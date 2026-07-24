ae136a1fe464465b7fb732836a95385acdf87d5e
Pradan-9928 <rpradan9928@gmail.com>
2026-07-24 15:20:31 +0530

    Array of Objects and Enhanced For loop

--- JULY_24TH.java
+++ JULY_24TH.java
@@ -1,7 +1,36 @@
-public class JULY_24TH {
-    
-    
-    public static void main(String[] args) {
-        
-    }
+//Question: Write a Java program that creates a class called Book with two attributes: Title and Price. 
+// In the main method, create an array of three Book objects, assign values to their attributes.
+// and then print out the details of each book.
+/* 
+class Book{
+    String Title;
+    int Price;
 }
+
+public class JULY_24TH {
+    public static void main(String[] args) {
+        Book[] book= new Book[3];
+
+    book[0]=new Book();
+    book[1]=new Book();
+    book[2]=new Book();
+
+    book[0].Title="Harry Potter";
+    book[0].Price=500;
+
+    book[1].Title="The Alchemist";
+    book[1].Price=300;
+
+    book[2].Title="The Great Gatsby";
+    book[2].Price=400;
+
+    for(Book b : book){
+        System.out.println("Book Title: " + b.Title);
+        System.out.println("Book Price: " + b.Price);
+
+    }
+    
+
+
+    }
+}*/
