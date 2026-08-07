c6039af78c410ba76521cdd76dd520c2002a9e6f
Pradan-9928 <rpradan9928@gmail.com>
2026-08-07 18:40:36 +0530

    String Builder and Buffer and String API

--- AUG_7TH.java
+++ AUG_7TH.java
@@ -1,7 +1,9 @@
 public class AUG_7TH{
     public static void main(String[] args){
+
+    //String Buffer Concept
          
-    StringBuffer name= new StringBuffer("Hello");
+    /*StringBuffer name= new StringBuffer("Hello");
     name.append(" Reddy");
     System.out.println(name.capacity());
     System.out.println(name.length());
@@ -9,6 +11,29 @@ public class AUG_7TH{
     System.out.println(name.indexOf("Reddy"));
     System.out.println(name.replace(0, 5, "Hi"));
     System.out.println(name.reverse());
-    System.out.println(name.delete(0, 3));
+    System.out.println(name.delete(0, 3));*/
+
+    //Comparing two strings using == and equals() method
+
+    /*String a = "Java";
+    String b = "Java";
+
+    System.out.println(a == b);
+    System.out.println(a.equals(b));
+
+    String a = new String("Java");
+    String b = new String("Java");
+
+    System.out.println(a == b);
+    System.out.println(a.equals(b));
+    
+    String a = "JAVA";
+    String b = "java";
+
+    System.out.println(a.equals(b));
+    System.out.println(a.equalsIgnoreCase(b));
+    System.out.println(a.compareTo(b));*/ 
+
+
     }
 }
