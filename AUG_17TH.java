1b2923ef7897e28c72b0db097794e5682da700ea
Pradan-9928 <rpradan9928@gmail.com>
2026-08-17 18:50:59 +0530

    Advanvced Pattern Printing

--- AUG_17TH.java
+++ AUG_17TH.java
@@ -1,23 +1,48 @@
-public class AUG_17TH{
-    public static void Butterfly_Pattern(int n){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
+import java.util.Scanner;
+
+public class AUG_17TH {
+
+    public static void Butterfly_Pattern(int n) {
+        // Upper Half
+        for (int i = 1; i <= n; i++) {
+            // Left stars
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            for(int k=1;k<=(n-i)*2;k++){
-                System.out.println();
+            // Middle spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
             }
-            for(int m=1;m<=n;m++){
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower Half
+        for (int i = n; i >= 1; i--) {
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Middle spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Right stars
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-    }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number of rows");
-        int rows=sc.nextInt();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = sc.nextInt();
         Butterfly_Pattern(rows);
         sc.close();
     }
+}
