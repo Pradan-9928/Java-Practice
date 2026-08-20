18bbc6ec2ecfc6a6792f00fc13cc157c351329b0
Pradan-9928 <rpradan9928@gmail.com>
2026-08-20 14:10:07 +0530

    Basic 6 problems of Loops and conditonal statements

--- AUG_20TH.java
+++ AUG_20TH.java
@@ -1,41 +1,70 @@
-public class AUG_20TH {
-    public static void printArray(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
+import java.util.*;
+public class AUG_20TH{
+    public static void main(String[] args ){
+        //TO CHECK THE LARGEST AMONG THREE NUMBERS
+        Scanner sc=new Scanner(System.in);
+        // System.out.print("Enter the number");
+        // int a =sc.nextInt();
+        // System.out.print("Enter the number");
+        // int b=sc.nextInt();
+        // System.out.print("Enter the number");
+        // int c=sc.nextInt();
 
-    }
-        
+        // int max=Math.max(c,Math.max(a, b));
+        // System.out.println(max);
 
-    public static void main(String[] args){
-        
-        //Bubble Sort
-        int[] arr={7,8,3,1,2};
-        // for(int i=0;i<arr.length-1;i++){
-        //     for(int j=0;j<arr.length-i-1;j++){
-        //         if (arr[j]>arr[j+1]){
-        //             int temp=arr[j];
-        //             arr[j]=arr[j+1];
-        //             arr[j+1]=temp;           
-        //         }
-        //     }
+        // //To check the case of the entered character
+        // System.out.println("Enter the word");
+        // String string=sc.next();
+
+        // char ch=string.trim().charAt(0);
+        // if(ch >= 'a' && ch <= 'b'){
+        //     System.out.println("the case is lower");
         // }
-        //Selection Sort
-        for(int k=0;k<arr.length-1;k++){
-            int smallest=k;
-            for(int l=k+1;l<arr.length;l++){
-                if(arr[smallest]>arr[l]){
-                    smallest=l;
-                }
-            int tempo=arr[smallest];
-            arr[smallest]=arr[k];
-            arr[k]=tempo;
 
-            }
-        }
-        printArray(arr); 
+        //Fibonacci Numbers
         
+        // System.out.println("How many times do you want to continue");
+        // int count=sc.nextInt();
+        // int p=0;
+        // int q=1;
+        // for(int i=0;i<=count;i++){
+        //     int temp=q;
+        //     q+=p;
+        //     p=temp;
+        //     System.out.println(q);
+            
+        // }
+        sc.close();
+        // System.out.println(q);
+
+        //Count the number of occurences
+        // int N=1924082;
+        // int occurences=0;
+        // while(N>0){
+        //     int rem=N%10;
+        //     if(rem==2){
+        //         occurences++;
+        //     }
+        //     N=N/10;
+        // }
+        // System.out.println(occurences);
+
+        //Reverse the number
+        int N=80373;
+        int reversed=0;
+        while(N!=0){
+            int temp=N%10;
+            reversed=(reversed*10)+temp;
+            N=N/10;
+        }
+        System.out.println(reversed);
+
+
+
+
+
+
+
     }
 }
-
-
